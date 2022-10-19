cb7a0e2823d9085afa843d6055a2d842e3742433
Marcos Azevedo <marcos.workspace@gmail.com>
2022-10-19 00:47:02 -0300

    feat: footer button to top

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,6 @@
 import { SocialIcon } from  'react-social-icons'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -17,20 +18,22 @@ export default function Header ({}: Props) {
         <SocialIcon url="https://github/m4rcosazevedo" fgColor="gray" bgColor="transparent" />
       </motion.div>
 
-      <motion.div
-        initial={{ x: 500, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1 }}
-        className="flex flex-row items-center text-gray-300 cursor-pointer"
-      >
-        <SocialIcon
-          className="cursor-pointer"
-          network="email"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">get in touch</p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{ x: 500, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">get in touch</p>
+        </motion.div>
+      </Link>
 
     </header>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import WorkExperience from '../components/work-experience'
 import Skills from '../components/skills'
 import Projects from '../components/projects'
 import ContactMe from '../components/contact-me'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
@@ -46,6 +47,18 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0"
+              src="https://i.imgur.com/e2yvD6A.png"
+              alt=""
+            />
+          </div>
+        </footer>
+      </Link>
 
     </div>
   )
