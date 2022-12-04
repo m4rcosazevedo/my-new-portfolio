8756b4398c4abc20768db655ddd39909dbb067c1
m4rcosazevedo <m4rcosazevedo@users.noreply.github.com>
2022-12-04 01:13:28 +0000

    deploy: 7c4264b1952fc8b53a43ccd664863323efcb8347

---
+++ _next/static/t0OULnU9vtONA0dAl9muj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/409-8b34e43754695322.js","static/chunks/pages/index-0de617c7c2bfa527.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
